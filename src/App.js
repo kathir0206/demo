d527624ad4bb2ca9e1d935e1178ac59728b441dc
kathir0206 <kathiresanmsc23@gmail.com>
2023-10-18 09:21:14 +0530

    try4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
+import React from "react";
 import "./App.css";
 import Dashboard from "./Dashboard/Dashboard";
-import Body from "./main/body/Body";
-import Footer from "./main/footer/Footer";
 import Header from "./main/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="content-container">
       <Dashboard />
-      <Body/>
+      <div className="content">
+        <Header />
       </div>
-      <Footer/>
-
     </div>
   );
 }
